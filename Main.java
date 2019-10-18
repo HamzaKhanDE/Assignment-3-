2ae436e78c8643147e78ea8561df7b0e407b69e4
Hamzak95 <55218814+Hamzak95@users.noreply.github.com>
2019-10-17 20:59:05 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        Apple apple = new Apple("Apple","sweet");
+
+        Orange orange = new Orange("Orange", "sour");
+
+        apple.eat();
+        orange.eat();
+
+    }
+}
